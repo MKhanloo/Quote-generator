112657aabe1d7d4d799a5de5fb6a8c3a351a746b
MKhanloo <m.amirkhanloo@outlook.com>
2023-05-07 18:50:48 +0330

    add new quote in script

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterQouteBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
+
 let apiQuotes = [];
+
+// show new quote
+function newQuote(){
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    quoteText.textContent = quote.text;
+    authorText.textContent = quote.author;
+}
 
 // Get quotes from API
 async function getQuotes(){
-    const apiUrl = "https://type.fit/api/quotes1";
+    const apiUrl = "https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+        newQuote();
     } catch(error){
         // Catch error here
         console.log("Error")
