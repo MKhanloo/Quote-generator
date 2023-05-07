25cb99afb61b8e9fdab1ea48dc71f10bbc5ce83c
MKhanloo <m.amirkhanloo@outlook.com>
2023-05-07 19:16:24 +0330

    blank author

--- script.js
+++ script.js
@@ -10,9 +10,16 @@ let apiQuotes = [];
 
 // show new quote
 function newQuote(){
+    // A ranndome quote from api quotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteText.textContent = quote.text;
-    authorText.textContent = quote.author;
+    // Check if author is blank and replace it with "Unknown"
+if (!quote.author){
+    authorText.textContent = "Unknown";
+}else{
+    authorText.textContent = quote.text;
+}
+authorText.textContent = quote.author;
+
 }
 
 // Get quotes from API
