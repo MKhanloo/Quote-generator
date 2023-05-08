9ae94ffd513c6ba3883a52f9fb3c0a87c3609d8d
MKhanloo <m.amirkhanloo@outlook.com>
2023-05-08 12:14:48 +0330

    Add loader

--- script.js
+++ script.js
@@ -3,32 +3,46 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterQouteBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
-
+const loader = document.getElementById("loader");
 
 
 let apiQuotes = [];
+//  Show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Hide loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // show new quote
 function newQuote(){
+
+    loading();
     // A ranndome quote from api quotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author is blank and replace it with "Unknown"
-if (!quote.author){
-    authorText.textContent = "Unknown";
-}else{
-    authorText.textContent = quote.author;
-}
-// Check quote length to determine styling
-if (quote.text.length > 100) {
-    quoteText.classList.add("long-quote");
-}else {
-    quoteText.classList.remove("long-quote");
-}
-quoteText.textContent = quote.text;
+    if (!quote.author){
+        authorText.textContent = "Unknown";
+    }else{
+        authorText.textContent = quote.author;
+    }
+    // Check quote length to determine styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add("long-quote");
+    }else {
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
+    complete()
 }
 
 // Get quotes from API
 async function getQuotes(){
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
